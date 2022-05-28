fb81c3b1ae37a71ac3a376e431a2be7664f5481a
sunlu <shunlu1019@163.com>
2022-05-28 14:20:05 +0800

    test2 hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest2.java
+++ src/main/java/com/atguigu/git/GitTest2.java
@@ -4,5 +4,6 @@ public class GitTest2 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello World2");
+        System.out.println("Hello hot-fix World2");
     }
 }
