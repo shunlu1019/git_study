f40c8f6366d59a8d665b9d694a5085bc9b88c5dd
sunlu <shunlu1019@163.com>
2022-05-28 13:59:26 +0800

    master commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,6 +5,8 @@ public class GitTest {
         System.out.println("Hello Git");
         System.out.println("Hello Git1");
         System.out.println("Hello Git2");
+        System.out.println("Hello Git5");
         System.out.println("Hello Git3");
+        System.out.println("master Git");
     }
 }
