1b9c18901d546ddd881ef337882243607d1c0b31
shunlu1019 <83901374+shunlu1019@users.noreply.github.com>
2022-05-28 14:54:19 +0800

    Update GitTest2.java

--- src/main/java/com/atguigu/git/GitTest2.java
+++ src/main/java/com/atguigu/git/GitTest2.java
@@ -5,6 +5,6 @@ public class GitTest2 {
         System.out.println("Hello World");
         System.out.println("Hello World2");
         System.out.println("Hello World3");
-        System.out.println("Hello hot-fix World2");
+        System.out.println("Hello hot-fix World");
     }
 }
